d99b7b6fc5c28ff0bb1270d93f1b6d431aecfc7e
Nikita Chalyi <nikita4556@gmail.com>
2024-05-05 14:48:11 +0300

    testing branch

--- src/test/java/ru/netology/service/CahbackHackServiseTest.java
+++ src/test/java/ru/netology/service/CahbackHackServiseTest.java
@@ -1,4 +1,24 @@
 package ru.netology.service;
-
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 public class CahbackHackServiseTest {
+
+
+    @Test
+    void calcCashbackEqual(){
+    CashbackHackService service =new CashbackHackService();
+int amount = 1000;
+int actual = service.remain(amount);
+int expected = 0;
+assertEquals(actual,expected);
+}
+@Test
+    void calcCashbackBelow(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual,expected);
+}
+
 }
